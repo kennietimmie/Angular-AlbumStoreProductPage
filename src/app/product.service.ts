7a10a0238cee1d2d4df8547d28650d187921425e
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2021-12-12 21:16:06 +0100

    getting album data

---
+++ src/app/product.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/operator/map';
+
+@Injectable()
+export class ProductService {
+
+  constructor(private _http: Http) { }
+  _albumUrl = '../assets/album.json';
+
+  getAlbum(id:number) {
+   return this._http.get(this._albumUrl)
+  }
+
+}
