f6ba65a1dbdb03f5a865cb2b9d4b081dd1641601
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2021-12-12 22:07:45 +0100

    product list

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,10 +3,11 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
-
+import { Product } from './product';
 @Injectable()
 export class ProductService {
  private  _albumUrl = '../assets/album.json';
+  private _productUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -14,4 +15,7 @@ export class ProductService {
    return this._http.get(this._albumUrl).map((response) => <Album>response.json())
   }
 
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productUrl).map(response => <Product[]>response.json())
+  }
 }
